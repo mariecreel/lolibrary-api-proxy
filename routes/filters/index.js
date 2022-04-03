b9f73d2dfc922d814db71be827a743344f0a102e
mariecreel <nick.creel@protonmail.com>
2022-04-02 21:46:38 -0400

    setting up jest (wip)

--- routes/filters/index.js
+++ routes/filters/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('get all filters');
-});
+    res.send('get all filters')
+})
 
 router.get('/:name', (req, res) => {
-    res.send(`get ${req.params['name']}`);
-});
+    res.send(`get ${req.params['name']}`)
+})
 
 router.post('/import', (req, res) => {
-    res.send(`import filter values`);
-});
+    res.send(`import filter values`)
+})
 
-module.exports = router;
+module.exports = router
