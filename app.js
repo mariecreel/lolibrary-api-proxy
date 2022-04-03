cf68d3eec6342a2f73a77926dfcf4cb62a5079d6
mariecreel <nick.creel@protonmail.com>
2022-04-02 23:02:47 -0400

    start setting up passport and auth0

--- app.js
+++ app.js
@@ -1,9 +1,54 @@
-require('dotenv').config();
-const express = require('express');
-const filters = require('./routes/filters/index');
-const app = express();
+/**
+ * required external modules
+ */
+require('dotenv').config()
+const express = require('express')
+const path = require("path")
+const expressSession = require("express-session")
+
+const passport = require("passport")
+/**
+ * app variables
+ */
+const app = express()
+const Auth0Strategy = require("passport-auth0")
+
+/**
+ * session config
+ */
+const session = {
+    process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false
+}
+
+if (app.get('env') === 'production') {
+    // serve secure cookies
+    session.cookie.secure = true;
+}
+
+/**
+ * passport config
+ */
+
+/**
+ * app config
+ */
+ app.set('views', path.join(__dirname, 'views'))
+ app.set('view engine', 'pug')
+ app.use(express.static(path.join(__dirname, 'public')))
+ 
+ app.use(expressSession(session))
+
+/**
+ * routers
+ */
+const filters = require('./routes/filters/index')
+const login = require('./routes/login/index')
 
 
-app.use('/filters', filters);
+app.use('/filters', filters)
+app.use('/login', login)
 
 app.listen(process.env.PORT || 3000, console.log("Server is running"));
