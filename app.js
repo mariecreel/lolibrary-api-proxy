d31e31eb4843b325835ab3f6de153c74ee044153
mariecreel <nick.creel@protonmail.com>
2022-04-02 23:31:22 -0400

    more auth0 setup (now login works)

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const { auth } = require('express-openid-connect')
 require('dotenv').config()
 const app = express()
 const filters = require('./routes/filters/index')
+
 // see https://auth0.github.io/express-openid-connect/
+// and https://auth0.com/docs/quickstart/webapp/express#integrate-auth0
 app.use(auth({
-    authRequired: false
+    authRequired: false,
+    auth0Logout: true, 
 }))
 
 app.use('/filters', filters)
+
+app.get('/', (req, res) => {
+    res.send(`hello ${req.oidc.user.sub}`)
+})
 
 app.listen(process.env.PORT || 3000, console.log("Server is running"));
