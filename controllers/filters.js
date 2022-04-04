30f22bc22a7779c2953e7577e5d9e33aceb3d196
mariecreel <nick.creel@protonmail.com>
2022-04-03 22:24:19 -0400

    setup skeleton for GET controllers

---
+++ controllers/filters.js
@@ -0,0 +1,12 @@
+const db = require('../models/index')
+
+// return all filters
+const allFiltersController = (req, res) => {
+    const filters = {}
+}
+
+const filterTypeController = (req, res) => {
+    const filter = {}
+    // check to see if filter is valid
+
+}
