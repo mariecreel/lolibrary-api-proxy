986ea350c28f49021eaae7f96dfaf217015747a5
mariecreel <nick.creel@protonmail.com>
2022-04-03 20:28:41 -0400

    fix mad imports

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
-const filters = require('routes/filters/index')
-const db = require('models/index.js')
+const filters = require('./routes/filters/index')
+const db = require('./models/index.js')
 
 app.use('/filters', filters)
 
