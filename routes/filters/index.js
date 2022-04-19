29d3caa3f281c5443f7e92ef0ced758ab9164605
mariecreel <nick.creel@protonmail.com>
2022-04-18 21:54:31 -0400

    setup validation for get/:name

--- routes/filters/index.js
+++ routes/filters/index.js
@@ -2,13 +2,38 @@ const express = require('express')
 const router = express.Router()
 const { checkJwt, checkScopes } = require('../../utility/auth')
 const importController = require('../../controllers/import')
+const { param, body, validationResult } = require('express-validator')
+
+const checkAllowedFilters = value => {
+    const _allowedFilters = {
+        'year': 1,
+        'colorway': 1,
+        'tags': 1, 
+        'brand': 1,
+        'category': 1,
+        'features': 1
+    }
+    return typeof _allowedFilters[value] !== 'undefined'
+}
 
 router.get('/', (req, res) => {
     res.send('get all filters')
 })
 
-router.get('/:name', (req, res) => {
+router.get('/:name',
+        param('name')
+             .exists()
+             .bail()
+             .isString()
+             .bail()
+             .custom(checkAllowedFilters),
+    (req, res) => {
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
     res.send(`get ${req.params['name']}`)
+    } else {
+        res.status(422).send()
+    }
 })
 router.post('/import', checkJwt, checkScopes, (req, res) => {
     importController(req, res)
