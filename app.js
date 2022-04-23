47f342123bbc444d54dcd557c8537aeab571a251
mariecreel <nick.creel@protonmail.com>
2022-04-23 19:43:19 -0400

    add rate limiting

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 require('dotenv').config()
+const rateLimit = require('express-rate-limit')
 const bodyParser = require('body-parser')
 const app = express()
 const filters = require('./routes/filters/index')
-const db = require('./models/index.js')
+
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // five minutes
+    max: 100,
+})
 
 app.use(bodyParser.json())
+app.use(limiter)
 app.use('/filters', filters)
 
 app.listen(process.env.PORT || 3000, console.log("Server is running"));
