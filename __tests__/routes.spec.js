af19cfa8fa999e6b3d36163a24e74316f7806a80
mariecreel <nick.creel@protonmail.com>
2022-04-23 20:23:40 -0400

    implement get for specific filters

--- __tests__/routes.spec.js
+++ __tests__/routes.spec.js
@@ -9,6 +9,18 @@ describe('get /filters', () => {
         expect(res.statusCode).toEqual(200)
         for (const filter of Object.keys(res.body)) {
             expect(checkAllowedFilters(filter)).toBe(true)
+            expect(res.body[filter].length).toBeGreaterThanOrEqual(1)
         }
+    })
+    describe('get individual filters', () => {
+        ['tags', 'colorway', 'year', 'features', 'category', 'brand'].forEach(filter => {
+            it(`get /filters/${filter}`, async () => {
+                const res = await request(app)
+                .get(`/filters/${filter}`)
+                expect(res.statusCode).toEqual(200)
+                expect(res.body).toHaveProperty(filter)
+                expect(res.body[filter].length).toBeGreaterThanOrEqual(1)
+            })
+        })
     })
 })
